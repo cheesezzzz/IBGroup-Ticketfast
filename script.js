e2baa879b57f0809d1a9f14e4ae817680293fa1a
cheesezzzz <huyng211203@gmail.com>
2023-02-26 17:31:45 +1100

    added the accordion expanding feature

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-const btnHamburger = document.querySelector('#hamburger-btn')
-const btnCloseMenu = document.querySelector('#menuClose')
-const menuSlider = document.querySelector('#menu-slider')
-const body = document.querySelector('#body')
+const btnHamburger = document.querySelector("#hamburger-btn");
+const btnCloseMenu = document.querySelector("#menuClose");
+const menuSlider = document.querySelector("#menu-slider");
+const accordionBtn = document.querySelectorAll("#accordion-btn");
+const accordionLinks = document.querySelectorAll("#accordion-links");
+const body = document.querySelector("#body");
 
 // !Menu Navigation
-// Displaying Menu Navigation function
+
+// Displaying Menu Navigation Slider Function
 const showNavigation = function () {
-  menuSlider.classList.toggle('-translate-x-full')
-  body.classList.toggle("overflow-hidden");
+  menuSlider.classList.remove("-translate-x-full");
+  body.classList.add("overflow-hidden");
+  document.style
 };
 
+// Closing the Menu Navigation Slider Function
+const closeNavigation = function () {
+  menuSlider.classList.add("-translate-x-full");
+  body.classList.remove("overflow-hidden");
+};
+
+// Hamburger click event to open menu navigation slider
 btnHamburger.addEventListener("click", function () {
-  // Displaying the Menu Navigation
   if (menuSlider.classList.contains("-translate-x-full")) {
     showNavigation();
   } else {
-    showNavigation();
+    closeNavigation();
   }
 });
 
-btnCloseMenu.addEventListener('click', () => {
-  showNavigation();
-})
+// Close menu navigation slider via button in the slider
+btnCloseMenu.addEventListener("click", () => {
+  closeNavigation();
+});
+
+// Closing the menu navigation slider when clicking outside 
+//TODO HELP: I need help with closing the navigation when clicking outside 
+// document.addEventListener('click', (e) => {
+//   if(e.target.id !== 'menu-slider' && e.target.id !== "menuClose") {
+//     closeNavigation();
+//   }
+// })
+
+// !Accordion Drop Down
+Array.from(accordionBtn).forEach((e) => {
+  e.addEventListener("click", () => {
+    e.nextElementSibling.classList.toggle("hidden");
+  });
+});
