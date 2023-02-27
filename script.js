b054d744afd89ec8b190d4dc4ff324343a157ea3
cheesezzzz <huyng211203@gmail.com>
2023-02-27 21:59:33 +1100

    updated category dropdown functionality

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ const btnCloseMenu = document.querySelector("#menuClose");
 const menuSlider = document.querySelector("#menu-slider");
 const accordionBtn = document.querySelectorAll("#accordion-btn");
 const accordionLinks = document.querySelectorAll("#accordion-links");
+const dropdownCategory = document.querySelector('#dropdown-category')
+const dropdownCategoryDetail = document.querySelector('#dropdown-category--detail')
+const dropdownButton = document.querySelector('#dropdown-button')
 const body = document.querySelector("#body");
 
-// !Menu Navigation
+// !Dropdown 
+document.addEventListener('click', (e) => {
+  console.log(e);
+  const isDropdownButton = e.target.closest('#dropdown-button')
 
+  if(isDropdownButton) {
+    dropdownCategoryDetail.classList.toggle('opacity-0')
+    dropdownCategoryDetail.classList.toggle('opacity-1')
+    dropdownCategoryDetail.classList.toggle('translate-y-0')
+  }
+  if(!isDropdownButton) {
+    dropdownCategoryDetail.classList.add('opacity-0')
+  }
+})
+
+
+
+// !Menu Navigation
 // Displaying Menu Navigation Slider Function
 const showNavigation = function () {
   menuSlider.classList.remove("-translate-x-full");
