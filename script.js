e9226adf809d2a40293d5beacad7b5a722f82a6a
cheesezzzz <huyng211203@gmail.com>
2023-02-27 23:45:20 +1100

    Completed dropdowns, search, responsiveness of nav

--- script.js
+++ script.js
@@ -3,23 +3,49 @@ const btnCloseMenu = document.querySelector("#menuClose");
 const menuSlider = document.querySelector("#menu-slider");
 const accordionBtn = document.querySelectorAll("#accordion-btn");
 const accordionLinks = document.querySelectorAll("#accordion-links");
-const dropdownCategory = document.querySelector('#dropdown-category')
 const dropdownCategoryDetail = document.querySelector('#dropdown-category--detail')
+const dropdownProfileDetail = document.querySelector('#dropdown-profile--detail')
 const dropdownButton = document.querySelector('#dropdown-button')
+const searchSlideIn = document.querySelector("#search-slidein")
+const searchBtn = document.querySelector('#search-btn')
 const body = document.querySelector("#body");
 
-// !Dropdown 
+// TODO HELP: How can I abstract/refactor the overall code so theres only one function for dropdown menus?
+
+// !Category Dropdown 
 document.addEventListener('click', (e) => {
-  console.log(e);
   const isDropdownButton = e.target.closest('#dropdown-button')
 
   if(isDropdownButton) {
     dropdownCategoryDetail.classList.toggle('opacity-0')
     dropdownCategoryDetail.classList.toggle('opacity-1')
+    dropdownCategoryDetail.classList.remove('invisible')
     dropdownCategoryDetail.classList.toggle('translate-y-0')
   }
   if(!isDropdownButton) {
     dropdownCategoryDetail.classList.add('opacity-0')
+    dropdownProfileDetail.classList.add('invisible')
+
+  }
+})
+
+// !Profile Dropdown
+document.addEventListener('click', (e) => {
+  const isDropdownButton = e.target.closest('#dropdown-button--profile')
+
+
+  // Dropdown opens when user clicks on an element with the closest parent element of the id #dropdown-button--profile
+  if(isDropdownButton) {
+    dropdownProfileDetail.classList.toggle('opacity-0')
+    dropdownProfileDetail.classList.toggle('opacity-1')
+    dropdownProfileDetail.classList.remove('invisible')
+    dropdownProfileDetail.classList.toggle('translate-y-0')
+  }
+
+  // Dropdown closes when users click outside of the closest mentioned parent element
+  if(!isDropdownButton) {
+    dropdownProfileDetail.classList.add('opacity-0')
+    dropdownProfileDetail.classList.add('invisible')
   }
 })
 
@@ -65,3 +91,8 @@ Array.from(accordionBtn).forEach((e) => {
     e.nextElementSibling.classList.toggle("hidden");
   });
 });
+
+// !Search slide in
+searchBtn.addEventListener('click', function() {
+  searchSlideIn.classList.toggle('max-h-44')
+})
